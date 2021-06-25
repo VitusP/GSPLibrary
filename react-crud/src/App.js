292a8597a9e1068ee75cb6ebe71312f676fb5ac9
vputra <vputra@cisco.com>
2021-06-25 08:59:02 -0400

    Add-book UI finctionality baseline

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { Link, Switch, Route } from 'react-router-dom';
+import AddBook from "./components/add-book.component";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/books" className="navbar-brand">
+            GSPLibrary
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/books"} className="nav-link">
+                Books
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/books"]} component={AddBook} />
+            <Route exact path="/add" component={AddBook} />
+            <Route path="/books/:id" component={AddBook} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
